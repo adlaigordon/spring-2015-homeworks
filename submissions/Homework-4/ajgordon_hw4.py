36a36eb7c3b90060a3c541c233b4b0a4cd227d3c
Adlai Gordon <adlai.gordon@gmail.com>
2015-03-23 22:22:27 -0400

    Kind of finished

---
+++ submissions/Homework-4/ajgordon_hw4.py
@@ -0,0 +1,12 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn.datasets as sk_data
+import sklearn.metrics as metrics
+from sklearn.cluster import KMeans
+
+
+#import matplotlib as mpl
+import seaborn as sns
+
